d5ce3c3e3bf5f1efa83900268dfea9a82d2d2ecd
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2024-11-20 10:35:12 -0800

    adding semantic tags

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,12 +4,12 @@ import AboutUs from "./AboutUs";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row justify-center items-center bg-orange-300 mt-auto ">
+    <footer className="flex flex-row justify-center items-center bg-orange-300 mt-auto ">
       <div>
         <Image src={"/avocado.png"} height={300} width={300} alt="avocado" />
       </div>
       <AboutUs />
-    </div>
+    </footer>
   );
 };
 
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center mt-10 mr-8 ml-8">
+    <section className="flex justify-center mt-10 mr-8 ml-8">
       <div className=" flex flex-col justify-center text-5xl lg:text-7xl relative tracking-wider">
         <div className="z-10">
           <h1>Keep Track & </h1>
@@ -17,7 +17,7 @@ const Hero = () => {
         </div>
         <div className="hero-border absolute z-0"></div>
       </div>
-    </div>
+    </section>
   );
 };
 
