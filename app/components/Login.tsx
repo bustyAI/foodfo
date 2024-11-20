d86421d68bb28f7413032b36b20ada8d40a3f82b
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2024-11-20 11:25:37 -0800

    creating form for login

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { FormEvent } from "react";
 
 // Components
 import { LoginInfo, Button } from "../components";
@@ -8,26 +9,45 @@ import { CiMail } from "react-icons/ci";
 import { IoEyeOutline } from "react-icons/io5";
 
 const Login = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    alert("Form submitted");
+  };
+
   return (
     <div className="overflow-hidden flex justify-center items-center min-h-screen min-w-full bg-orange-300 ">
       <div className="flex flex-col items-center justify-center mb-40 w-full bg-white rounded-xl m-8 p-4">
         <h1 className="text-4xl font-semibold mb-12">Login</h1>
-        <LoginInfo Icon={CiMail} loginType="Email" />
-        <LoginInfo Icon={IoEyeOutline} loginType="Password" />
-        <div className=" w-3/4 flex justify-end">
-          <h1 className="text-orange-500 underline hover:text-blue-500 hover:cursor-pointer">
-            Forgot Password?
-          </h1>
-        </div>
-        <Button
-          classParams="w-3/5"
-          borderColor="border-orange-500"
-          textColor="text-orange-500"
-          buttonText="Login"
-          buttonHoverColor="bg-orange-500"
-          textHoverColor="text-white"
-        />
-        <div className="flex items-center justify-between w-full mt-10">
+        <form onSubmit={handleSubmit} className="w-full">
+          <LoginInfo
+            Icon={CiMail}
+            loginType="Email"
+            inputType="text"
+            placeHolder="savemoney123@gmail.com"
+            inputId="email"
+          />
+          <LoginInfo
+            Icon={IoEyeOutline}
+            loginType="Password"
+            inputType="text"
+            placeHolder="keep this secret"
+            inputId="password"
+          />
+          <div className="flex w-5/6 justify-end">
+            <h1 className="text-orange-500 underline hover:text-blue-500 hover:cursor-pointer">
+              Forgot Password?
+            </h1>
+          </div>
+          <Button
+            classParams="w-3/5"
+            borderColor="border-orange-500"
+            textColor="text-orange-500"
+            buttonText="Login"
+            buttonHoverColor="bg-orange-500"
+            textHoverColor="text-white"
+          />
+        </form>
+        <div className="flex items-center justify-between w-full mt-10 mb-4">
           <span className="flex-1 border-t border-slate-300"></span>
           <span className=" text-sm mx-4 text-center text-slate-500">
             Or Continue With
--- app/components/LoginInfo.tsx
+++ app/components/LoginInfo.tsx
@@ -3,16 +3,36 @@ import React from "react";
 interface LoginInfoProps {
   Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   loginType: string;
+  inputType?: string;
+  placeHolder?: string;
+  inputId: string;
 }
 
-const LoginInfo = ({ Icon, loginType }: LoginInfoProps) => {
+const LoginInfo = ({
+  Icon,
+  loginType,
+  inputType,
+  placeHolder,
+  inputId,
+}: LoginInfoProps) => {
   return (
     <div className="flex flex-col w-full items-center mb-4">
       <div className="flex flex-col w-3/4 space-y-2">
-        <h1 className="text-left text-sm font-medium">{loginType}</h1>
+        <label htmlFor={inputId} className="text-left text-sm font-medium">
+          {loginType}
+        </label>
         <div className="flex flex-row border-2 border-slate-200 rounded-lg p-2">
-          <Icon className="mr-2" />
+          <Icon
+            className="mr-2 mt-1
+          "
+          />
           <div className=" border-r-2 border-slate-200 rounded-lg"></div>
+          <input
+            autoFocus={true}
+            type={inputType}
+            placeholder={placeHolder}
+            className="flex-1 outline-none border-none px-2"
+          />
         </div>
       </div>
     </div>
