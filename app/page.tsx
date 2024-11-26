2f1c513836511782ff344dd3c5ad35bcf172680d
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2024-11-26 11:23:19 -0800

    Moving navbar to appear on all pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Navbar, Hero, Button, Footer } from "@/app/components";
 export default function Home() {
   return (
     <main className=" min-h-screen flex flex-col overflow-hidden">
-      <Navbar />
       <Hero />
 
       <a href={"/api/auth/login"}>
